ce3ed262fe582a8d81a68d3e39c7b2a49bf8c642
Franklin Ho <fho@twitter.com>
2014-09-24 19:46:43 -0700

    Initial Commit

---
+++ TwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by Franklin Ho on 9/24/14.
+//  Copyright (c) 2014 Franklin Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
